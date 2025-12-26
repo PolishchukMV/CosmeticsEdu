76d8e741d833d4b3d493735ab52f98758124e72c
PolishchukMV <Xenx3025@yandex.ru>
2025-12-27 01:28:16 +0500

    Initial Commit

--- CosmeticsEdu/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('shop.urls')),
-    path('', include('django.contrib.auth.urls')),  # Добавляет login, logout, password_change и т.д.
-]
